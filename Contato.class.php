21ab5fa283697a1c813ddb1581f6d42243fccb03
Thiago Bomfim <40004059+thbomfim@users.noreply.github.com>
2026-02-14 12:46:23 -0300

    metodo mostrarContato, deletarContato adicionado e atualizar contato adicionado parcialmente com bugs que irei corrigir

--- Contato.class.php
+++ Contato.class.php
@@ -10,10 +10,10 @@ class Contato {
 
     function __construct($dbConnection) {
         $this->pdo = $dbConnection;
-        $this->id = $_POST["id"];
-        $this->nome = $_POST["nome"];
-        $this->celular = $_POST["celular"];
-        $this->email = $_POST["email"];
+        $this->id = $_POST["id"] ?? 'null';
+        $this->nome = $_POST["nome"] ?? 'null';
+        $this->celular = $_POST["celular"] ?? 'nul';
+        $this->email = $_POST["email"] ?? 'null';
     }
 
     public function adicionarContato() {
@@ -38,10 +38,6 @@ class Contato {
         $stmt->bindParam(":celular", $this->celular);
         $stmt->bindParam(":email", $this->email);
 
-        var_dump($this->nome);
-        var_dump($this->celular);
-        var_dump($this->email);
-
         $stmt->execute();
 
         if ($stmt->rowCount() > 0 ) {
@@ -53,8 +49,78 @@ class Contato {
     }
 
     public function mostrarContatos() {
+        $sql = "SELECT * FROM agenda";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->fetchAll();
 
+        foreach ($result as $chave => $valor) {
+            ?>
+            <li><strong> Nome :</strong><?= $valor['nome'] ?></li>
+            <li><strong> celular :</strong><?= $valor['celular'] ?></li>
+            <li><strong> Email :</strong><?= $valor['email'] ?></li>
+            <a href="index.php?pg=deletarContato&id=<?=$valor['id']?>">[X]</a>
+            <a href="index.php?pg=alterarContato&id=<?=$valor['id']?>&nome=<?=$valor['nome']?>&celular=<?=$valor['celular']?>&email=<?=$valor['email']?>">[A]</a>
+            <hr>
+            <?php
+            }
+    }  
+    
+    public function deletarContato() {
+        $id = $_GET["id"];
+
+    //verificar se não o contato existe
+        $sql = "SELECT * FROM agenda WHERE id = :id";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(":id", $id);
+        $stmt->execute();
+        $result = $stmt->fetch();
+
+        if (!$result) {
+            die("Este contato não existe!");
+        }
+
+        //deletar contato
+        $sql = "DELETE FROM agenda WHERE id = :id";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(":id", $id);
+        $stmt->execute();
+
+        if ($stmt->rowCount() > 0) {
+            echo 'Contato apagado!';
+        } else {
+            echo 'Ocorreu algum erro!';
+        }
     }
+
+     public function alterarContato() {
+        $id = $_GET["id"];
+
+    //verificar se não o contato existe
+        $sql = "SELECT * FROM agenda WHERE id = :id";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(":id", $id);
+        $stmt->execute();
+        $result = $stmt->fetch();
+
+        if (!$result) {
+            die("Este contato não existe!");
+        }
+
+        //atualiza as informacoes do contato
+        $sql = "UPDATE agenda set nome = :nome, celular = :celular, email = :email WHERE id = :id";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(":nome", $nome);
+        $stmt->bindParam(":celular", $celular);
+        $stmt->bindParam(":email", $email);
+        $stmt->execute();
+
+        if ($stmt->rowCount() > 0) {
+            echo 'Contato alterado com sucesso!';
+        } else {
+            echo 'Ocorreu algum erro!';
+        }
+     }
 
 
 }
