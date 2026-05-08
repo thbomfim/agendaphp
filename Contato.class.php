9ad9e608a13c978b1510570495d0a6eeb8b22ebf
Thiago Bomfim <40004059+thbomfim@users.noreply.github.com>
2026-05-08 11:04:00 -0300

    alguns ajuste

--- Contato.class.php
+++ Contato.class.php
@@ -1,82 +1,93 @@
-<?php 
-    include "config.php";
-
-    class Contato 
-    {
-        public $id;
-        public $nome;
-        public $celular;
-        public $email;
-        public $pdo;
-
-        function __construct($dbConnection) 
-        {
-            $this->pdo = $dbConnection;
-            $this->id = $_POST["id"] ?? 'null';
-            $this->nome = $_POST["nome"] ?? 'null';
-            $this->celular = $_POST["celular"] ?? 'nul';
-            $this->email = $_POST["email"] ?? 'null';
-        }
-
-        public function adicionarContato() 
-        {
-            //verifica se o contato ja existe e nao permite adicionar outro contato caso algum dado se repetir
-            $sql = "SELECT 1 FROM agenda WHERE nome = :nome || celular = :celular || email = :email LIMIT 1";
-            $stmt = $this->pdo->prepare($sql);
-            $stmt->bindParam(":nome", $this->nome);
-            $stmt->bindParam(":celular", $this->celular);
-            $stmt->bindParam(":email", $this->email);
-            $stmt->execute();
-            $result = $stmt->fetch();
-
-            if ($result) 
-            {
-                die("Um dos dados desse contato ja existe");
-            }
-
-            //insert do contato
-            $sql = "INSERT INTO agenda (nome, celular, email) VALUES (:nome, :celular, :email)";
-
-            $stmt = $this->pdo->prepare($sql);
-            $stmt->bindParam(":nome", $this->nome);
-            $stmt->bindParam(":celular", $this->celular);
-            $stmt->bindParam(":email", $this->email);
-
-            $stmt->execute();
-
-            if ($stmt->rowCount() > 0 ) 
-            {
-                echo 'Contato Adicionado com Suceso!';
-            }else
-            {
-                echo 'Ocorreu algum erro!';
-            }
-
-        }
-
-        public function mostrarContatos() 
-        {
-            $sql = "SELECT * FROM agenda";
-            $stmt = $this->pdo->prepare($sql);
-            $stmt->execute();
-            $result = $stmt->fetchAll();
-
-            foreach ($result as $chave => $valor) 
-                {
-?>
-                    <li><strong> Nome: </strong><?= $valor['nome'] ?></li>
-                    <li><strong> celular: </strong><?= $valor['celular'] ?></li>
-                    <li><strong> Email: </strong><?= $valor['email'] ?></li>
-                    <a href="index.php?pg=deletarContato&id=<?=$valor['id']?>" onclick="return confirm('Tem certeza que deseja excluir estes dados?')">[X]</a> |
-                    <a href="index.php?pg=alterarContato&id=<?=$valor['id']?>">[A]</a>
-                    <hr>
 <?php
-                }
-        }   
-        
-        public function deletarContato() 
-        {
-            try {
+include "config.php";
+
+class Contato
+{
+    public $id;
+    public $nome;
+    public $celular;
+    public $email;
+    public $pdo;
+
+    function __construct($dbConnection)
+    {
+        $this->pdo = $dbConnection;
+        $this->id = $_POST["id"] ?? 'null';
+        $this->nome = $_POST["nome"] ?? 'null';
+        $this->celular = $_POST["celular"] ?? 'nul';
+        $this->email = $_POST["email"] ?? 'null';
+    }
+
+    public function adicionarContato()
+    {
+        //verifica se os inputs foram digitados
+        switch (true) {
+            case empty($this->nome):
+                echo 'Digite um nome';
+                return;
+            case empty($this->celular):
+                echo 'Digite um celular';
+                return;
+            case empty($this->email):
+                echo 'Digite um email';
+                return;
+            default: 
+                echo 'alguns dos dados foram digitados errados';
+                return;
+        }
+
+        //verifica se o contato ja existe e nao permite adicionar outro contato caso algum dado se repetir
+        $sql = "SELECT 1 FROM agenda WHERE nome = :nome || celular = :celular || email = :email LIMIT 1";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(":nome", $this->nome);
+        $stmt->bindParam(":celular", $this->celular);
+        $stmt->bindParam(":email", $this->email);
+        $stmt->execute();
+        $result = $stmt->fetch();
+
+        if ($result) {
+            die("Um dos dados desse contato ja existe");
+        }
+
+        //insert do contato
+        $sql = "INSERT INTO agenda (nome, celular, email) VALUES (:nome, :celular, :email)";
+
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(":nome", $this->nome);
+        $stmt->bindParam(":celular", $this->celular);
+        $stmt->bindParam(":email", $this->email);
+
+        $stmt->execute();
+
+        if ($stmt->rowCount() > 0) {
+            echo 'Contato Adicionado com Suceso!';
+        } else {
+            echo 'Ocorreu algum erro!';
+        }
+    }
+
+    public function mostrarContatos()
+    {
+        $sql = "SELECT * FROM agenda";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+
+        foreach ($result as $chave => $valor) {
+?>
+            <li><strong> Nome: </strong><?= $valor['nome'] ?></li>
+            <li><strong> celular: </strong><?= $valor['celular'] ?></li>
+            <li><strong> Email: </strong><?= $valor['email'] ?></li>
+            <a href="agenda.php?pg=deletarContato&id=<?= $valor['id'] ?>" onclick="return confirm('Tem certeza que deseja excluir estes dados?')">[X]</a> |
+            <a href="agenda.php?pg=alterarContato&id=<?= $valor['id'] ?>">[A]</a>
+            <hr>
+<?php
+        }
+    }
+
+    public function deletarContato()
+    {
+        try {
             $id = $_GET["id"];
 
             //verificar se não o contato existe
@@ -86,8 +97,7 @@
             $stmt->execute();
             $result = $stmt->fetch();
 
-            if (!$result) 
-            {
+            if (!$result) {
                 die("Este contato não existe!");
             }
 
@@ -97,73 +107,29 @@
             $stmt->bindParam(":id", $id);
             $stmt->execute();
 
-            if ($stmt->rowCount() > 0) 
-            {
+            if ($stmt->rowCount() > 0) {
                 echo 'Contato apagado!';
             }
-            
-            }catch (PDOException $e)
-            {
-                echo 'Ocorreu algum erro! :' .$e->getMessage();
-            }
-            catch (Exception $e) 
-            {
-                echo 'Ocorreu algum erro:' .$e->getMessage();
-            }
+        } catch (PDOException $e) {
+            echo 'Ocorreu algum erro! :' . $e->getMessage();
+        } catch (Exception $e) {
+            echo 'Ocorreu algum erro:' . $e->getMessage();
         }
+    }
 
-        public function alterarContato($id) {
-            try 
-            {
-                $id = $_POST['id'];
-                $nome = $_POST['nome'];
-                $celular = $_POST['celular'];
-                $email = $_POST['email'];
+    public function alterarContato($id)
+    {
+        try {
+            $id = $_POST['id'];
+            $nome = $_POST['nome'];
+            $celular = $_POST['celular'];
+            $email = $_POST['email'];
 
-                var_dump($id);
-                var_dump($nome);
-                var_dump($celular);
-                var_dump($email);
+            var_dump($id);
+            var_dump($nome);
+            var_dump($celular);
+            var_dump($email);
 
-                //verificar se não o contato existe
-                $sql = "SELECT * FROM agenda WHERE id = :id";
-                $stmt = $this->pdo->prepare($sql);
-                $stmt->bindParam(":id", $id);
-                $stmt->execute();
-                $result = $stmt->fetch();
-
-                if (!$result) 
-                {
-                    //ao invez do die lancei em uma excecao para cair no catch
-                    throw new Exception("Este contato não existe!");
-                }
-
-                //atualiza as informacoes do contato
-                $sql = "UPDATE agenda set nome = :nome, celular = :celular, email = :email WHERE id = :id";
-                $stmt = $this->pdo->prepare($sql);
-                $stmt->execute([
-                    ':nome' => $nome,
-                    ':celular' => $celular,
-                    ':email' => $email,
-                    ':id' => $id
-                ]);
-
-                    echo 'Contato alterado com sucesso!';
-            } 
-            catch (PDOException $e) 
-            {
-                //captura os erros de sintaxe SQL ou conexao
-                echo "Erro no banco de dados: " .$e->getMessage();
-            } 
-            catch (Exception $e) 
-            {
-                // Captura o erro do "contato não existe" ou outros erros manuais
-                echo "Error: " . $e->getMessage();
-            }
-        }
-
-        public function buscarDados($id) 
-        {
             //verificar se não o contato existe
             $sql = "SELECT * FROM agenda WHERE id = :id";
             $stmt = $this->pdo->prepare($sql);
@@ -171,23 +137,53 @@
             $stmt->execute();
             $result = $stmt->fetch();
 
-            if (!$result) 
-            {
-                die("Este contato não existe!");
+            if (!$result) {
+                //ao invez do die lancei em uma excecao para cair no catch
+                throw new Exception("Este contato não existe!");
             }
 
-            //busca os dados
-            $sql = "SELECT * FROM agenda WHERE id = :id";
+            //atualiza as informacoes do contato
+            $sql = "UPDATE agenda set nome = :nome, celular = :celular, email = :email WHERE id = :id";
             $stmt = $this->pdo->prepare($sql);
-            $stmt->bindParam(":id", $id);
-            $stmt->execute();
-            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+            $stmt->execute([
+                ':nome' => $nome,
+                ':celular' => $celular,
+                ':email' => $email,
+                ':id' => $id
+            ]);
 
-            return $result;
+            echo 'Contato alterado com sucesso!';
+        } catch (PDOException $e) {
+            //captura os erros de sintaxe SQL ou conexao
+            echo "Erro no banco de dados: " . $e->getMessage();
+        } catch (Exception $e) {
+            // Captura o erro do "contato não existe" ou outros erros manuais
+            echo "Error: " . $e->getMessage();
+        }
+    }
 
+    public function buscarDados($id)
+    {
+        //verificar se não o contato existe
+        $sql = "SELECT * FROM agenda WHERE id = :id";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(":id", $id);
+        $stmt->execute();
+        $result = $stmt->fetch();
+
+        if (!$result) {
+            die("Este contato não existe!");
         }
 
+        //busca os dados
+        $sql = "SELECT * FROM agenda WHERE id = :id";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(":id", $id);
+        $stmt->execute();
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
 
+        return $result;
     }
+}
 
 ?>
--- index.php
+++ index.php
@@ -9,11 +9,11 @@
 
 ?>
     <header>
-        <h1 id="logo">Agenda de contatos</h1><br>
+    <h1 id="logo">Agenda de contatos</h1><br>
     </header>
 
     <div class="divcentro">
-        <form action="index.php?pg=adicionarContato" method="post"><br>
+        <form action="agenda.php?pg=adicionarContato" method="post"><br>
             <div class="campo">
                 <label for="nome">Nome: </label><input type="text" name="nome"/><br>
             </div>
@@ -26,47 +26,4 @@
             <button type="submit">Enviar</button>
         </form>
         <br>
-        <a href="?pg=contatos">Mostrar Contatos</a><br><hr>
-
-<?php
-        if ($pg == "adicionarContato") 
-        {
-            $Contato->adicionarContato();
-        }
-        elseif ($pg == "contatos") 
-        {
-            $Contato->mostrarContatos();
-        }
-        elseif ($pg == "deletarContato") 
-        {
-            $Contato->deletarContato();
-        }
-        elseif ($pg == "alterarContato") 
-        {
-            $id = $_GET['id'] ?? '';
-            $buscarContato = $Contato->buscarDados($id);
-?>
-                <form action="index.php?pg=alterarContatook" method="post">
-                    <input type="hidden" name="id" value="<?=$buscarContato['id']?>"/>
-                    <div class="campo">
-                        <label for="nome">Nome: </label><input type="text" name="nome" value="<?=$buscarContato['nome']?>"/><br>
-                    </div>
-                    <div class="campo">
-                        <label for="celular">Celular: </label><input type="number" name="celular" value="<?=$buscarContato['celular']?>"/><br>
-                    </div>
-                    <div class="campo">
-                        <label for="email">Email: </label><input type="email" name="email" value="<?=$buscarContato['email']?>"/><br>
-                    </div>
-                    <button type="submit">Enviar</button>
-                </form>
-        <?php
-        }
-        elseif ($pg == "alterarContatook") 
-        {
-            $id = $_POST['id'] ?? '';
-            $Contato->alterarContato($id);
-        }
-
-        include 'footer.html';
-?>
-    </div>
+        <a href="agenda.php?pg=contatos">Mostrar Contatos</a><br><hr>
